27d24366504ee3192dcfe75e9be1397e52c04a77
idd_vguimaraes <idd_guimaraes@uolinc.com>
2021-11-21 18:37:12 -0400

    [FEATURE][AUTH/START] - Create quth start screen - Navigate to login

--- feature-auth/src/main/java/com/drawiin/feature_auth/AuthNavGraph.kt
+++ feature-auth/src/main/java/com/drawiin/feature_auth/AuthNavGraph.kt
@@ -8,27 +8,44 @@ import androidx.navigation.compose.composable
 import androidx.navigation.navigation
 import com.drawiin.core.arch.NavigationRoute
 import com.drawiin.feature_auth.presentation.onboard.OnboardScreen
+import com.drawiin.feature_auth.presentation.start.AuthStartScreen
 import com.google.accompanist.pager.ExperimentalPagerApi
 
 @ExperimentalPagerApi
 fun NavGraphBuilder.addAuthNavGraph(navHostController: NavHostController, routeName: String){
-    navigation(route = routeName, startDestination = AuthRoutes.Onboard.routeName){
-        composable(AuthRoutes.Onboard.routeName){
-            OnboardScreen { navHostController.navigate(AuthRoutes.Start.routeName)}
+    navigation(route = routeName, startDestination = AuthRoutes.Onboard.routeName) {
+        composable(AuthRoutes.Onboard.routeName) {
+            OnboardScreen(
+                onGoToAuthStart = {
+                    navHostController.navigate(AuthRoutes.Start.routeName)
+                }
+            )
         }
 
-        composable(AuthRoutes.Start.routeName){
-            Text(text = "Auth/Start", style = MaterialTheme.typography.h3)
+        composable(AuthRoutes.Start.routeName) {
+            AuthStartScreen(
+                onGoToLogin = {
+                   navHostController.navigate(AuthRoutes.LogIn.routeName)
+                },
+                onGoToSignup = {
+                    navHostController.navigate(AuthRoutes.SignUp.routeName)
+                }
+            )
         }
 
-        composable(AuthRoutes.SignUp.routeName){
+        composable(AuthRoutes.SignUp.routeName) {
             Text(text = "Auth/Signup", style = MaterialTheme.typography.h3)
+        }
+
+        composable(AuthRoutes.LogIn.routeName) {
+            Text(text = "Auth/LogIn", style = MaterialTheme.typography.h3)
         }
     }
 }
 
 sealed class AuthRoutes(override val routeName: String): NavigationRoute {
-    object Onboard: AuthRoutes("auth/onboard")
-    object Start: AuthRoutes("auth/start")
-    object SignUp: AuthRoutes("auth/signup")
+    object Onboard : AuthRoutes("auth/onboard")
+    object Start : AuthRoutes("auth/start")
+    object SignUp : AuthRoutes("auth/signup")
+    object LogIn : AuthRoutes("auth/login")
 }
